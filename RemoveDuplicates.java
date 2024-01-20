67667474a143350d3b5666fb4e2d135cca1d65d1
HanaAyman33 <126604344+HanaAyman33@users.noreply.github.com>
2024-01-20 23:51:23 +0200

    Add files via upload

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -1,42 +1,44 @@
 import java.util.*;
+/*Given a string s, remove duplicate letters so that 
+ * every letter appears once and only once. 
+ * You must make sure your result is
+the smallest in lexicographical order
+ among all possible results.
+ */
 public class RemoveDuplicates {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();  //length of the array
-		int[] num = new int[n];
-		for(int i = 0;i<n;i++)
-			num[i] = sc.nextInt();
-		int[] expected = new int[getLength(num,0)];
-		getExpected(expected,num);
-		int k = getLength(num,0);
-		for(int i = 0;i<expected.length;i++)
-			System.out.print(expected[i]+" ");
-	}
-	public static int getLength(int[] nums,int count){
-		boolean flag = true;
-        for(int i = 0;i<nums.length;i++) {
-        	for(int j = 0;j<nums.length & i!=j;j++) {
-        		if(nums[i]==nums[j]) {
-        			flag = false;
-            		break;
-        		}
-        	}
-        	if(flag)
-        		count++;
-        }
-        return count;
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine();
+        String result = removeDuplicateLetters(s);
+        System.out.println(result);
+        input.close();
     }
-	public static void getExpected(int[] n,int[] num) {
-		boolean flag = false;
-        for(int i = 0;i<n.length;i++){
-        	for(int j = 0;j<n.length & i!=j;j++) {
-        		if(n[i]!=n[j])
-        			flag = true;
-        	}
-        	if(flag) {
-        		num[i] = n[i];
-        		flag = false;
-        	}
+    public static String removeDuplicateLetters(String s) {
+        int n = s.length();
+        int[] count = new int[26];
+        boolean[] visited = new boolean[26];
+        for (int i = 0; i < n; i++) {
+            count[s.charAt(i) - 'a']++;
         }
-	}
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            count[c - 'a']--;
+            if (visited[c - 'a']) {
+                continue;
+            }
+            while (!stack.isEmpty() && stack.peek() > c
+            		&& count[stack.peek() - 'a'] > 0) {
+                visited[stack.peek() - 'a'] = false;
+                stack.pop();
+            }
+            stack.push(c);
+            visited[c - 'a'] = true;
+        }
+        StringBuilder result = new StringBuilder();
+        while (!stack.isEmpty()) {
+            result.append(stack.pop());
+        }
+        return result.reverse().toString();
+    }
 }
